00a52f0acf06ea9c73cb10a47ad6be890b029a30
andyro2 <a.rodan@campus.technion.ac.il>
2020-05-14 13:43:18 +0300

    excel comparison

--- src/train_locally_dcn.py
+++ src/train_locally_dcn.py
@@ -4,7 +4,6 @@ import  torchvision
 import  time
 import  os
 import  copy
-import pydicom
 import  torch.nn            as nn
 import  torch.optim         as optim
 import  numpy               as np
